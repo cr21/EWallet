949ac220c7ed675e825ac0be05624270f4550406
Chirag Tagadiya <cr.tagadiya@gmail.com>
2022-03-30 21:32:18 -0400

    Delete e-wallet directory

--- e-wallet/transaction-service/src/main/java/com/example/transactionservice/TransactionStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.transactionservice;
-
-public enum TransactionStatus {
-    PENDING,
-    SUCCESS,
-    FAILED
-}
--- e-wallet/user-service/src/main/java/com/example/ewallet/users/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ewallet.users;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository  extends JpaRepository<User, Integer> {
-}
